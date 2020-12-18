832f28fcdf783cb7a0b3019e8e38211be6bc52ef
ihsankursadunal <32536146+ihsankursadunal@users.noreply.github.com>
2020-12-18 23:20:44 +0300

    Update e-voting.sol
    ERENKUN THANKS

--- e-voting.sol
+++ e-voting.sol
@@ -1,66 +1,50 @@
 pragma solidity >=0.5.1<0.8.0;
 
 contract EVOTING{
-    
+
    address owner;
+
     struct Voter {
         bool isVoted;
         uint8 my_candidate;
     }
-    
-    struct candidate {
+
+    struct Candidate {
         uint voteCount;
     }
-    
+
     mapping(address=>Voter) voters;
-    candidate[] candidates;
-    
+    Candidate[] candidates;
+
     constructor (uint candidate_num) public {
         owner = msg.sender;
         for(uint i = 0 ; i<candidate_num; i++){
-            candidates.push(candidate(0));
+            candidates.push(Candidate(0));
         }
     }
-    
-    modifier notOwner(){
-        require(msg.sender != owner);
-        _;
+
+    function vote(uint8 index) public {
+        require(!voters[msg.sender].isVoted, "You can vote only once");
+        require(index < candidates.length, "Invalid candidate");
+        voters[msg.sender].isVoted = true;
+        voters[msg.sender].my_candidate = index;
+        candidates[index].voteCount += 1;
     }
-    
-    modifier notVoted(){
-        require(!voters[msg.sender].isVoted);
-        _;
-    }
-    
-    
-    function vote(uint8 index) public notVoted {
-        if(index >= candidates.length){
-            return;
-        }else{
-            voters[msg.sender].isVoted = true;
-            voters[msg.sender].my_candidate = index;
-            candidates[index].voteCount += 1;  
-        }
-    }
-        
-    function atTheMoment(uint a) public view returns(int){
-        if(a>=candidates.length){
-            return -1;
-        }
+
+    function getCandidateVoteCount(uint a) public view returns(int){
+        require(a < candidates.length, "Invalid candidate");
         return int(candidates[a].voteCount);
     }
-    
-    function result() public view returns (uint256 winer, uint8 my_candidate) {
+
+    function result() public view returns (uint256 winner) {
         uint256 max = 0;
-        winer=0;
+        winner=0;
         for (uint8 cand = 0; cand < candidates.length;cand++){
             if (candidates[cand].voteCount > max){
                 max = candidates[cand].voteCount;
-                winer = cand;
+                winner = cand;
             }
-    
         }
-        my_candidate = voters[msg.sender].my_candidate;
     }
-    
+
 }
